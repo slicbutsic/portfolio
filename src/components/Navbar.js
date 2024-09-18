70a9c52e670ad007e26a21956158cdf1532001a8
Lucas Siviglia <siviglialucas@gmail.com>
2024-09-18 10:04:06 +1000

    updating navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,49 @@
+import { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars } from '@fortawesome/free-solid-svg-icons';
-
+import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
 
 export const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  // Function to toggle the sidebar
+  const toggleSidebar = () => {
+    setIsOpen(!isOpen);
+  };
 
   return (
     <>
-      <div className='bg-white w-16 h-16 rounded-full ml-12 mt-8 flex items-center justify-center'>
+      {/* Menu Icon */}
+      <div
+        className='bg-white w-16 h-16 rounded-full ml-12 mt-8 flex items-center justify-center cursor-pointer'
+        onClick={toggleSidebar}
+      >
         <FontAwesomeIcon className='h-5' icon={faBars} />
       </div>
+
+      {/* Sidebar */}
+      <div
+        className={`fixed top-0 left-0 h-full bg-white transition-transform transform ${
+          isOpen ? 'translate-x-0' : '-translate-x-full'
+        } w-64`}
+      >
+        <div className='text-black p-4'>
+          {/* Close Button */}
+          <div className='flex justify-end'>
+            <button onClick={toggleSidebar}>
+              <FontAwesomeIcon className='text-black h-5' icon={faTimes} />
+            </button>
+          </div>
+
+          {/* Sidebar Content */}
+          {/* <h1 className='text-lg font-bold mb-6'>Menu</h1> */}
+          <ul>
+            <li className='mb-4'><a href='#'>Home</a></li>
+            <li className='mb-4'><a href='#'>About</a></li>
+            <li className='mb-4'><a href='#'>Contact</a></li>
+            <li className='mb-4'><a href='#'>Services</a></li>
+          </ul>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
