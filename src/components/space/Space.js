cee17055030315723d9be5e23aa37ae2356dbf65
Lucas Siviglia <siviglialucas@gmail.com>
2024-09-23 12:25:16 +1000

    update about

--- src/components/space/Space.js
+++ src/components/space/Space.js
@@ -1,22 +1,14 @@
 import React from 'react';
 import StarfieldAnimation from 'react-starfield';
 import './space.css';
-// import { Hero } from '../hero/Hero';
-// import { Projects } from '../projects/Projects';
+
 
 
 export const Space = () => {
   return (
     <div className="starfield-container">
       <StarfieldAnimation
-        // numParticles={1000}
-        // depth={1500}
-        // speed={0.8}
-        // lineWidth={1.2}
-        // starRadius={0.7}
       />
-      {/* <Projects /> */}
-
     </div>
   );
 };
